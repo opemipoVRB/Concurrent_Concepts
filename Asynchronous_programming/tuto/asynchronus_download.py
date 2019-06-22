b99a3306dc7c9c5b0e66223c745a64195a53abd7
opemipoVRB <opemipodurodola@gmail.com>
2019-06-22 01:24:37 +0100

    initialize repo

---
+++ Asynchronous_programming/tuto/asynchronus_download.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
+←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
+↓↓...........................................................................↓↓
+↓↓..........................↓↓↓↓↓↓↓↓↓↓↓↓↓....................................↓↓
+↓↓.......................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.................................↓↓
+↓↓.....................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓...............................↓↓
+↓↓....................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.↓↓...............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓...↓↓..............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.↓↓...↓↓↓.............................↓↓
+↓↓...................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..............................↓↓
+↓↓....................↓↓↓↓↓↓↓↓↓↓↓↓↓.....↓↓↓↓↓↓↓↓↓............................↓↓
+↓↓......................↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..↓↓↓↓↓↓↓............................↓↓
+↓↓...................................↓↓↓.....................................↓↓
+↓↓.................↓↓................↓↓↓↓ ↓↓↓↓↓↓↓........↓...................↓↓
+↓↓...............↓↓↓↓↓↓..............↓↓↓↓↓↓↓↓↓↓↓↓↓...↓↓↓↓↓↓..................↓↓
+↓↓............↓↓↓↓..↓↓↓↓↓.........................↓↓↓↓↓↓↓↓↓..................↓↓
+↓↓............↓↓↓↓...↓↓↓↓↓↓↓....................↓↓↓↓↓↓.↓↓.↓↓.................↓↓
+↓↓...............↓↓↓↓↓↓↓↓↓↓↓↓↓↓............↓↓↓↓↓↓↓↓..........................↓↓
+↓↓.........................↓↓↓↓↓↓↓↓↓...↓↓↓↓↓↓↓...............................↓↓
+↓↓..............................↓↓↓↓↓↓↓↓↓↓...................................↓↓
+↓↓..........................↓↓↓↓↓....↓↓↓↓↓↓↓↓↓...............................↓↓
+↓↓............↓↓.↓↓↓↓↓↓↓↓↓↓↓↓↓............↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓..................↓↓
+↓↓............↓↓.↓↓..↓↓↓↓.....................↓↓↓↓↓↓↓↓↓↓↓↓↓↓.................↓↓
+↓↓..............↓↓↓↓↓↓............................↓↓.↓↓↓↓↓↓↓.................↓↓
+↓↓..................                                   ......................↓↓
+↓↓.................. ↑↑↑  ↑↑↑  ↑↑↑↑↑↑↑        ↑↑↑↑↑↑↑ .......................↓↓
+↓↓.................. ↑↑↑  ↑↑↑  ↑↑↑   ↑↑↑↑     ↑↑↑   ↑↑↑↑.....................↓↓
+↓↓.................. ↑↑↨  ↑↑↑  ↑↑↨   ↨↑↑      ↑↑↨   ↨↑↑......................↓↓
+↓↓.................. ↨↑↨  ↑↨↑  ↨↑↨   ↨↑↨      ↨↑↨   ↨↑↨......................↓↓
+↓↓.................. ↑↨↑  ↨↑↨  ↨↨↑↨↑↨↨↑↑↨     ↨↨↑↨↑↨↨↑↑↨.....................↓↓
+↓↓.................. ↨↑↨  ↨↨↨  ↨↨↨      ↨↨↨   ↨↨↨     ↨↨↨....................↓↓
+↓↓.................. :↨:  ↨↨:  ↨↨:      :↨↨   ↨↨:     :::....................↓↓
+↓↓................... ::↨↨:↨   :↨:      :↨:   :↨:     :::....................↓↓
+↓↓.................... ::::    :::      :::   :::     :::....................↓↓
+↓↓...................... :      :        :      :::::::  ....................↓↓
+↓↓...........................................................................↓↓
+↓↓←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←↓↓
+↓↓→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→↓↓
+↓↓      asynchronus_download.py  Created by  Durodola Opemipo 2019           ↓↓
+↓↓            Personal Email : <opemipodurodola@gmail.com>                   ↓↓
+↓↓                 Telephone Number: +2348182104309                          ↓↓
+↓↓→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→↓↓
+↓↓←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←↓↓
+
+"""
+
+import logging
+import os
+import sys
+import time
+from queue import Queue
+from threading import Thread
+
+from Asynchronous_programming.tuto.download import collect_links, create_download_dir, download_link
+
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#
+# logger = logging.getLogger(__name__)
+
+
+class DownloadWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            directory, link = self.queue.get()
+            try:
+                download_link(directory, link)
+            finally:
+                self.queue.task_done()
+
+
+def main():
+    ts = time.time()
+    website_1 = "http://icecat.biz/p/toshiba/pscbxe-01t00een/satellite-pro-notebooks-4051528049077-Satellite+Pro+C8501GR-17732197.html"
+    website_2 = "https://unsplash.com/search/photos/hd"
+    download_dir = create_download_dir()
+    links = [*collect_links(website_2), *collect_links(website_1)]
+    # Setup progress bar
+    # sys.stdout.write("Asynchronous Download |---> [%s]" % (" " * len(links)))
+    # sys.stdout.flush()
+    # sys.stdout.write("\b" * (len(links) + 1))  # return to start of line, after '['
+    progress = 0
+    index = 0
+    status = ""
+    operation_type = "Asynchronous"  #
+    barLength = 100
+    #
+    # Create a queue to communicate with the woker threads
+    queue = Queue()
+    # Create 8 worker threads
+    for x in range(8):
+        worker = DownloadWorker(queue)
+        # Setting daemon to True will let the main thread exit even though the workers are blocking
+        worker.daemon = True
+        worker.start()
+
+    # Put the tasks into the queue as a tuple
+
+    for link in links:
+        # logger.info('Queueing {}'.format(link))
+        queue.put((download_dir, link))
+        index = index + 1
+        progress = index / len(links)
+        # Launch ProgressBar
+
+        if progress >= 1:
+            progress = 1
+            status = "Done...\r\n"
+        block = int(round(barLength * progress))
+        # print(progress)  # Proof that progress bar works but
+        text = "\r {0} Download|--->[{1}] Percent: {2}% {3}".format(operation_type,
+                                                                    "#" * block + "-" * (barLength - block),
+                                                                    progress * 100, status)
+        sys.stdout.write(text)
+        time.sleep(0.1)
+        sys.stdout.flush()
+    # Causes the main thread to wait for the queue to finish processing all the tasks
+    queue.join()
+    info = 'Took {} seconds to download {} files'.format(time.time() - ts, len(links))
+    sys.stdout.write("]\n " + info)  # this ends the progress bar
+
+
+if __name__ == '__main__':
+    main()
